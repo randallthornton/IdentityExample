e2f9b30fb59f8e0e9544af3df699e15cb6b17f48
Randall Thornton <randall.thornton@universalorlando.com>
2023-01-15 13:56:40 -0500

    Add project files.

---
+++ Identity/Config.cs
@@ -0,0 +1,29 @@
+﻿using IdentityServer4.Models;
+
+namespace Identity
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope>
+        {
+            new ApiScope("monolith", "My API")
+        };
+
+        public static IEnumerable<Client> Clients => new List<Client>
+        {
+            new Client
+            {
+                ClientId = "gateway",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new Secret("gatewaysecret".Sha256())
+                },
+                AllowedScopes =
+                {
+                    "monolith"
+                }
+            }
+        };
+    }
+}
---
+++ Identity/Program.cs
@@ -0,0 +1,29 @@
+using Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddIdentityServer()
+    .AddDeveloperSigningCredential()
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddInMemoryClients(Config.Clients);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseIdentityServer();
+
+app.Run();
